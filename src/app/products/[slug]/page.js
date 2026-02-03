86ba96d6a31e4a6f053f5d1a513e6ce4e2deddcd
Otterseas <info@otterseas.com>
2026-02-03 13:13:22 +0800

    Update page.js

--- src/app/products/[slug]/page.js
+++ src/app/products/[slug]/page.js
@@ -214,6 +214,7 @@ export default function ProductPage() {
               Otterseas
             </span>
           </Link>
+          <CurrencySwitcher />
         </header>
 
         {/* Main Content */}
